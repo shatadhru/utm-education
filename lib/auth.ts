ab8a670baf29ee7647d534d55b8c48626d4a1408
shatadhru <ssnmggs@gmail.com>
2026-08-19 13:15:19 +0600

    v3

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,13 +2,14 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { admin } from "better-auth/plugins"
+import { sendEmail } from "./email/send";
 
 const client = new MongoClient(process.env.DATABASE_URL!);
 const db = client.db();
 
 export const auth = betterAuth({
   database: mongodbAdapter(db),
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://v1.utmeducation.com",
   emailAndPassword: { enabled: true },
   socialProviders: {
     google: {
@@ -27,5 +28,22 @@ export const auth = betterAuth({
 	"http://localhost:*", // Any localhost port
 	"exp://**", // Expo development URLs
 	"myapp://", // Mobile app scheme
-]
+],
+
+emailVerification: {
+          sendOnSignUp: true,
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            void sendEmail({
+                to: user.email,
+                subject: 'Verify your email address',
+                html: `Click the link to verify your email: ${url}`
+            })
+        }
+    } ,
+    account: {
+		accountLinking: {
+			enabled: true,
+			trustedProviders: ["google"], // Add your providers
+		},
+	},
 });
